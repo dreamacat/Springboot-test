29d83f99fd2d667df5312932d2cb9660de1667fc
wangxiaoqiang <wangxiaoqiang@souche.com>
2017-07-10 13:31:54 +0800

    添加Mybatis，待调整

--- testboot-site/src/main/java/com/cat/Application.java
+++ testboot-site/src/main/java/com/cat/Application.java
@@ -18,6 +18,8 @@ public class Application {
         return "Hello World!";
     }
     public static void main(String[] args) {
+
         SpringApplication.run(Application.class, args);
+
     }
 }
